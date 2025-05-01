ce2096dd8f6dfb9b90a6517aadf3f60f895b1346
yoadria <tuadria@gmail.com>
2025-05-01 19:20:32 +0200

    establecidas rutas correctamente

--- main.py
+++ main.py
@@ -3,17 +3,9 @@ import logging
 from database.conexion import get_db_connection
 from database.tablas_db import crear_tabla_llamadas
 _logging = logging.getLogger(__name__)
+from app import create_app
+app = create_app()
 
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return send_from_directory('html', 'index.html')
-
-def inicio():
-    conn = get_db_connection()
-    if conn:
-        crear_tabla_llamadas(conn)
 
 if __name__ == '__main__':
-    inicio()
     app.run(debug = True, host="0.0.0.0") #Que no sea visible para todo el mundo - para eso usar un host ="0.0.0.0"
